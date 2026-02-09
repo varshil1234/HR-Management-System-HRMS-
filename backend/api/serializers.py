137ede387b410a1d5d05dd43e57c2caca1981f63
KrishTCC <krishbpatel0648@gmail.com>
2026-02-09 17:10:19 +0530

    Force add src files

--- backend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class LoginSerializer(serializers.Serializer):
-    employee_id = serializers.CharField() # Changed from username
-    password = serializers.CharField()
-
-class ResetPasswordSerializer(serializers.Serializer):
-    employee_id = serializers.CharField() # Changed from username
-    new_password = serializers.CharField()
